6268eb6365dac659697476340553f2a67987f638
deeeee654 <ebele7630@gmail.com>
2026-03-15 10:16:19 +0000

    ui

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">The lounge</h1>
-      <div>
-        <div></div>
-        <div></div>
+    <div className="min-h-screen w-full p-8">
+      <h1 className="text-3xl text-center font-bold mb-8">The lounge</h1>
+      <div className="flex gap-8 w-full">
+        <div className="flex flex-col gap-4 w-1/3">
+          <ul className="flex flex-col gap-4">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Genres</a>
+            </li>
+            <li>
+              <a href="#">Top 100</a>
+            </li>
+            <li>
+              <a href="#">New Releases</a>
+            </li>
+            <li>
+              <a href="#">Chat</a>
+            </li>
+            <li>
+              <a href="#">Discover</a>
+            </li>
+          </ul>
+        </div>
+        <div className="w-2/3">Stories</div>
       </div>
     </div>
   );
